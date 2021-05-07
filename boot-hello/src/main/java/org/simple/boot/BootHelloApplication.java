967ea8ca7aebcbb48b2772e3e4ce05934994fa78
wangzhao <13132398914@163.com>
2021-05-07 11:38:15 +0800

    添加以SpringApplicationBuilder方式启动SpringBoot应用

--- boot-hello/src/main/java/org/simple/boot/BootHelloApplication.java
+++ boot-hello/src/main/java/org/simple/boot/BootHelloApplication.java
@@ -1,12 +1,20 @@
 package org.simple.boot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BootHelloApplication {
 
     public static void main(String[] args) {
+        /*
+        SpringApplicationBuilder builder=new SpringApplicationBuilder(BootHelloApplication.class);
+        builder.bannerMode(Banner.Mode.OFF);
+        SpringApplication application=builder.build();
+        application.run(args);
+         */
         SpringApplication.run(BootHelloApplication.class, args);
     }
 
