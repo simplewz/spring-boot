06ac829951a008df4aabf67e1a4b387b169cce81
wangzhao <13132398914@163.com>
2021-05-11 22:07:47 +0800

    添加获取SpringBoot应用中的组件

--- boot-hello/src/main/java/org/simple/boot/BootHelloApplication.java
+++ boot-hello/src/main/java/org/simple/boot/BootHelloApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BootHelloApplication {
@@ -15,7 +16,13 @@ public class BootHelloApplication {
         SpringApplication application=builder.build();
         application.run(args);
          */
-        SpringApplication.run(BootHelloApplication.class, args);
+        //1.返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(BootHelloApplication.class, args);
+        //2.获取容器中的所有组件
+        String[] names=run.getBeanDefinitionNames();
+        for(String name:names){
+            System.out.println(name);
+        }
     }
 
 }
